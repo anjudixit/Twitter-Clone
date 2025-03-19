5c792c783b1bd2d089918f7c522150fc81951102
anjudixit <163434535+anjudixit@users.noreply.github.com>
2025-03-19 20:00:04 +0530

    script.js

--- script.js
+++ script.js
@@ -7,23 +7,3 @@ left.addEventListener("click",()=>{
 right.addEventListener("click",()=>{
     line.style.left='60%';
 })
-
-const themeToggle = document.querySelector(".themeToggle");
-const htmlElement = document.documentElement;
-
-// Check if the user already has a saved theme
-if (localStorage.getItem("theme") === "dark") {
-    htmlElement.classList.add("dark");
-}
-
-// Toggle theme on button click
-themeToggle.addEventListener("click", () => {
-    htmlElement.classList.toggle("dark");
-
-    // Save theme preference in localStorage
-    if (htmlElement.classList.contains("dark")) {
-        localStorage.setItem("theme", "dark");
-    } else {
-        localStorage.setItem("theme", "light");
-    }
-});
